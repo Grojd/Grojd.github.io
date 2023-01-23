59f0188e265ab12eea48d53894f06f7028d1be02
VoKo <31620263+voko@users.noreply.github.com>
2023-01-23 17:02:35 +0100

    filters

--- app/js/fe.js
+++ app/js/fe.js
@@ -1,8 +1,14 @@
 const filter = document.querySelector('.filtering');
 const filterFixed = document.querySelector('.fixed');
+const filtersContainer = document.querySelector('.filters-container');
+const exitBtn = document.querySelector('.exit');
 
 const observer = new IntersectionObserver(entry => {
     filterFixed.classList.toggle('hidden', entry.isIntersecting)
 })
 
 observer.observe(filter);
+
+filter.addEventListener('click', () => filtersContainer.classList.add('offscreen'));
+filterFixed.addEventListener('click', () => filtersContainer.classList.add('offscreen'));
+exitBtn.addEventListener('click', () => filtersContainer.classList.remove('offscreen'));
